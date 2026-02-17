1e46ec10da6e09a01c53daba0d28ad93ec8c9662
Suhas_20 <suhaskavade3654@gmail.com>
2026-02-18 00:33:04 +0530

    Solve the problem Revese Vowels in the String

--- DSA/Problems/Program2.java
+++ DSA/Problems/Program2.java
@@ -1,5 +1,51 @@
 package DSA.Problems;
 
+/*
+    . Greatest Common Divisor of Strings
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+
+Output: "ABC"
+
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+
+Output: ""
+
+Example 4:
+
+Input: str1 = "AAAAAB", str2 = "AAA"
+
+Output: ""​​​​​​​
+
+ 
+
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+*/
+
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
 
